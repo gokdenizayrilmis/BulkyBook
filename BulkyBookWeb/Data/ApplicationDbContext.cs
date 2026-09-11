6a5f3f69153018e1c0cbc10e4468f2e69de19c25
Gokdeniz <ayrilmis.gokdeniz@gmail.com>
2026-09-11 20:06:23 +0300

    added changes

---
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace BulkyBookWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){}
+
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
